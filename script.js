313d109f49ac48f6f4dd832c701aa34f9aeb556e
Rachel Moore <moorer@gcsnc.com>
2020-10-12 11:48:45 -0400

    a commit while I try to work out problems with query url

--- script.js
+++ script.js
@@ -21,38 +21,38 @@ function postList() {
   userSelectedCity.push(userInput);
   console.log(userSelectedCity);
   
-  document.getElementById("currCity").innerHTML = userSelectedCity + "&nbsp";
+  
   localStorage.setItem("userInput", JSON.stringify(userSelectedCity));
   for (let i = 0; i < userSelectedCity.length; i++) {
   
   console.log(userSelectedCity[i]);
   document.getElementById(cityList[i]).innerHTML = userSelectedCity[i];
-  
+  document.getElementById("currCity").innerHTML = userSelectedCity[i] + "&nbsp";
   console.log("cityList", cityList[i]);
-  document.getElementById(currCity).innerHTML = userSelectedCity[i];
 };
 };
 
  // This is our API key
- var APIKey = "166a433c57516f51dfab1f7edaed8413";
+ var APIKey = "bb55783c8d612e6ce0920236575bef0d";
 
  // Here we are building the URL we need to query the database
- var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + APIKey;
+ var queryURL = "api.openweathermap.org/data/2.5/find?q=userSelectedCity[i]&appid=bb55783c8d612e6ce0920236575bef0d"
+//  console.log(queryURL);
 
- // Here we run our AJAX call to the OpenWeatherMap API
-//  $.ajax({
-//    url: queryURL,
-//    method: "GET"
-//  })
+//  Here we run our AJAX call to the OpenWeatherMap API
+ $.ajax({
+   url: queryURL,
+   method: "GET"
+ })
 
-   // We store all of the retrieved data inside of an object called "response"
-  //  .then(function(response) {
+  //  We store all of the retrieved data inside of an object called "response"
+   .then(function(response) {
 
-     // Log the queryURL
-    //  console.log(queryURL);
+  //   //  Log the queryURL
+     console.log(queryURL);
 
-     // Log the resulting object
-    //  console.log(response);
+  //   //  Log the resulting object
+     console.log(response);
 
      // Transfer content to HTML
     //  $(".city").html("<h1>" + response.name + " Weather Details</h1>");
@@ -70,7 +70,7 @@ function postList() {
   //    console.log("Wind Speed: " + response.wind.speed);
   //    console.log("Humidity: " + response.main.humidity);
   //    console.log("Temperature (F): " + tempF);
-  //  });
+   });
 
 // Format Dates in Any Timezone
 // var jun = moment("2014-06-01T12:00:00Z");
