d6c29e5f30f5b622bd5e34dce0755fa3fbc147d1
Rachel Moore <moorer@gcsnc.com>
2020-10-13 08:56:54 -0400

    logging lat and lon for uv index ajax call

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ function postList() {
         $("#wind").text("Wind Speed: " + data.list[0].wind.speed);
         $("#humidity").text("Humidity: " + data.list[0].main.humidity);
         $("#currDate").text("Date: " + date);
+        console.log('humidity', data.list[0].main.humidity)
 
         //  Convert the temp to fahrenheit
         var tempF = (data.list[0].main.temp - 273.15) * 1.80 + 32;
@@ -60,14 +61,40 @@ function postList() {
         //  $("#temp").text("Temperature (K) " + data.list[0].main.temp);
         $("#temp").text("Temperature (F) " + tempF.toFixed(2));
 
-        // Log the data in the console as well
-        console.log("Wind Speed: " + data.list[0].main.wind.speed);
-        console.log("Humidity: " + data.list[0].main.humidity);
-        //  console.log("Temperature (F): " + tempF);
+        var lat = (data.list[0].coord.lat);
+        var lon = (data.list[0].coord.lon);
+        console.log("lat: ", lat, "lon ", lon);
+
       })
   }
 }
 
+
+// Gets UV index
+var latt = lat;
+var long = lon;
+console.log("latt", lat, "long", lon);
+// var queryUV = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=bb55783c8d612e6ce0920236575bef0d"
+
+// function UV() {
+//   for (let i = 0; i < userSelectedCity.length; i++) {
+//     var queryUV = "https://api.openweathermap.org/data/2.5/forecast?q=" + userSelectedCity[i] + "&appid=bb55783c8d612e6ce0920236575bef0d";
+
+//     //  Here we run our AJAX call to the OpenWeatherMap API
+//     $.ajax({
+//       url: queryURL,
+//       method: "GET"
+//     })
+//       .then(function (data) {
+//         console.log("response", data)
+
+//       })
+//   }
+// }
+// function UV()
+
+
+// gets 5 day forecast
 function fiveDay() {
   for (let i = 0; i < userSelectedCity.length; i++) {
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userSelectedCity[i] + "&appid=bb55783c8d612e6ce0920236575bef0d";
