9e27a0e5d5396ab9f783d98f899f9d7169035283
Rachel Moore <moorer@gcsnc.com>
2020-10-15 18:16:46 -0400

    got the uv index working

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
 var date = moment().format("MMM Do YYYY");
+var day = moment().format("MMM Do");
 var hour = parseInt(moment().format("HH"));
 var cityList = ["city1", "city2", "city3", "city4", "city5", "city6", "city7", "city8"]
 var userSelectedCity = [];
-
+var lat = 0;
+var lon = 0;
 
 function postData() {
   var localStore = JSON.parse(localStorage.getItem("userInput"))
@@ -47,7 +49,7 @@ function postList() {
       method: "GET"
     })
 
-      //  We store all of the retrieved data inside of an object called "response"
+      //  We store all of the retrieved data inside of an object called "data"
       .then(function (data) {
         console.log("response", data)
         $("#wind").text("Wind Speed: " + data.list[0].wind.speed + " Mph");
@@ -63,86 +65,99 @@ function postList() {
         // add temp content to html
         //  $("#temp").text("Temperature (K) " + data.list[0].main.temp);
         $("#temp").text("Temperature " + tempF.toFixed(0) + "°F");
-
+        lat = (data.list[0].coord.lat);
+        lon = (data.list[0].coord.lon);
+        uvData()
       })
   }
 }
 
 // -------------------------------Gets UV index------------------------------------------
 
-//    function uvData() {
-
-   
-//     var queryUV = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=bb55783c8d612e6ce0920236575bef0d"
-//   .then(function (uvData) {
-//     console.log("uvData", uvData);
-//     var lat = (data.list[0].coord.lat);
-//     var lon = (data.list[0].coord.lon);
-//     console.log("lat: ", lat, "lon ", lon);
-//   }
-    
-// }
-
-      // --------------------------gets 5 day forecast-------------------------------------------
-      function fiveDay() {
-        for (let i = 0; i < userSelectedCity.length; i++) {
-          var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userSelectedCity[i] + "&appid=bb55783c8d612e6ce0920236575bef0d";
-
-          //  Here we run our AJAX call to the OpenWeatherMap API
-          $.ajax({
-            url: queryURL,
-            method: "GET"
-          })
-            .then(function (data) {
-              console.log("response", data)
-
-              // --------------Five Day Forecast Weather: Day 1 -----------------
-              $("#date1").text(data.list[3].dt_txt);
-              $("#hum1").text("Humidity: " + data.list[3].main.humidity + "%");
-              var icon = "http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png";
-              $("#icon1").attr("src", icon);
-              //  Convert the temp to fahrenheit
-              var tempF = (data.list[3].main.temp - 273.15) * 1.80 + 32;
-              $("#temp1").text("Temperature: " + tempF.toFixed(0) + "°F");
+   function uvData() {
 
 
-              // --------------Five Day Forecast Weather: Day 2 -----------------
-              $("#date2").text(data.list[11].dt_txt);
-              $("#hum2").text("Humidity: " + data.list[11].main.humidity + "%");
-              var icon = "http://openweathermap.org/img/w/" + data.list[11].weather[0].icon + ".png";
-              $("#icon2").attr("src", icon);
-              //  Convert the temp to fahrenheit
-              var tempF = (data.list[11].main.temp - 273.15) * 1.80 + 32;
-              $("#temp2").text("Temperature: " + tempF.toFixed(0) + "°F");
+    var queryUV = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=bb55783c8d612e6ce0920236575bef0d";
 
-              // --------------Five Day Forecast Weather: Day 3 -----------------
-              $("#date3").text(data.list[18].dt_txt);
-              $("#hum3").text("Humidity: " + data.list[18].main.humidity + "%");
-              var icon = "http://openweathermap.org/img/w/" + data.list[18].weather[0].icon + ".png";
-              $("#icon3").attr("src", icon);
-              //  Convert the temp to fahrenheit
-              var tempF = (data.list[18].main.temp - 273.15) * 1.80 + 32;
-              $("#temp3").text("Temperature: " + tempF.toFixed(0) + "°F");
+        //  Here we run our AJAX call to the OpenWeatherMap API
 
-              // --------------Five Day Forecast Weather: Day 4 -----------------
-              $("#date4").text(data.list[26].dt_txt);
-              $("#hum4").text("Humidity: " + data.list[26].main.humidity + "%");
-              var icon = "http://openweathermap.org/img/w/" + data.list[26].weather[0].icon + ".png";
-              $("#icon4").attr("src", icon);
-              //  Convert the temp to fahrenheit
-              var tempF = (data.list[26].main.temp - 273.15) * 1.80 + 32;
-              $("#temp4").text("Temperature: " + tempF.toFixed(0) + "°F");
+    $.ajax({
+      url: queryUV,
+      method: "GET"
+    })
+    .then(function (data) {
+      console.log("response", data)
+    $("#uv").text("UV Index:" + data.value);
+  })
 
-              // --------------Five Day Forecast Weather: Day 5 -----------------
-              $("#date5").text(data.list[34].dt_txt);
-              $("#hum5").text("Humidity: " + data.list[34].main.humidity + "%");
-              var icon = "http://openweathermap.org/img/w/" + data.list[34].weather[0].icon + ".png";
-              $("#icon5").attr("src", icon);
-              //  Convert the temp to fahrenheit
-              var tempF = (data.list[34].main.temp - 273.15) * 1.80 + 32;
-              $("#temp5").text("Temperature: " + tempF.toFixed(0) + '°F');
+}
 
-            })
-        }
-      }
-      fiveDay()
+
+// --------------------------gets 5 day forecast-------------------------------------------
+function fiveDay() {
+  for (let i = 0; i < userSelectedCity.length; i++) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userSelectedCity[i] + "&appid=bb55783c8d612e6ce0920236575bef0d";
+
+    //  Here we run our AJAX call to the OpenWeatherMap API
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+
+        // --------------Five Day Forecast Weather: Day 1 -----------------
+        
+        .then(function (data) {
+          console.log("response", data)
+
+        // $("#date1").text(data.list[2].dt_txt);
+        // var day = moment().add(1, 'days').calendar();
+        // var day = moment().format("MMM Do");
+
+        $("#hum1").text("Humidity: " + data.list[2].main.humidity + "%");
+        var icon = "http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png";
+        $("#icon1").attr("src", icon);
+        //  Convert the temp to fahrenheit
+        var tempF = (data.list[2].main.temp - 273.15) * 1.80 + 32;
+        $("#temp1").text("Temperature: " + tempF.toFixed(0) + "°F");
+
+
+        // --------------Five Day Forecast Weather: Day 2 -----------------
+        $("#date2").text(data.list[10].dt_txt);
+        $("#hum2").text("Humidity: " + data.list[11].main.humidity + "%");
+        var icon = "http://openweathermap.org/img/w/" + data.list[10].weather[0].icon + ".png";
+        $("#icon2").attr("src", icon);
+        //  Convert the temp to fahrenheit
+        var tempF = (data.list[10].main.temp - 273.15) * 1.80 + 32;
+        $("#temp2").text("Temperature: " + tempF.toFixed(0) + "°F");
+
+        // --------------Five Day Forecast Weather: Day 3 -----------------
+        $("#date3").text(data.list[18].dt_txt);
+        $("#hum3").text("Humidity: " + data.list[18].main.humidity + "%");
+        var icon = "http://openweathermap.org/img/w/" + data.list[18].weather[0].icon + ".png";
+        $("#icon3").attr("src", icon);
+        //  Convert the temp to fahrenheit
+        var tempF = (data.list[18].main.temp - 273.15) * 1.80 + 32;
+        $("#temp3").text("Temperature: " + tempF.toFixed(0) + "°F");
+
+        // --------------Five Day Forecast Weather: Day 4 -----------------
+        $("#date4").text(data.list[26].dt_txt);
+        $("#hum4").text("Humidity: " + data.list[26].main.humidity + "%");
+        var icon = "http://openweathermap.org/img/w/" + data.list[26].weather[0].icon + ".png";
+        $("#icon4").attr("src", icon);
+        //  Convert the temp to fahrenheit
+        var tempF = (data.list[26].main.temp - 273.15) * 1.80 + 32;
+        $("#temp4").text("Temperature: " + tempF.toFixed(0) + "°F");
+
+        // --------------Five Day Forecast Weather: Day 5 -----------------
+        $("#date5").text(data.list[34].dt_txt);
+        $("#hum5").text("Humidity: " + data.list[34].main.humidity + "%");
+        var icon = "http://openweathermap.org/img/w/" + data.list[34].weather[0].icon + ".png";
+        $("#icon5").attr("src", icon);
+        //  Convert the temp to fahrenheit
+        var tempF = (data.list[34].main.temp - 273.15) * 1.80 + 32;
+        $("#temp5").text("Temperature: " + tempF.toFixed(0) + '°F');
+
+      })
+  }
+}
+fiveDay()
